a8aee24ec63e9207bcbdafbe5f1159615c0af81f
Olepeponcio <olepepinsan@gmail.com>
2026-08-01 13:46:52 +0200

    implementacion de clases Jugada, Piedra, Tijeras, Papel

---
+++ src/piedra_papel_tijeras/models/tijeras.py
@@ -0,0 +1,25 @@
+from .jugada import Jugada
+
+
+class Tijeras(Jugada):
+    """clase del tipo Jugada
+    args:
+      name
+
+    def:
+      __str__()
+      vence_a()
+    """
+
+    def __init__(self, name: str = "Tijeras") -> None:
+        """inicializa los atributos de la instancia de clase heredada"""
+
+        super().__init__(name)
+
+    def __str__(self) -> str:
+        return f"Jugada: {self.name}"
+
+    def vence_a(self, otra: Jugada) -> bool:
+        """devuelve true si el nombre de la otra jugada es piedra
+        return: bool"""
+        return otra.__str__ == "Papel"
