80382f47d96c2ace4118f0e38b28b9facfc3485c
Olepeponcio <olepepinsan@gmail.com>
2026-08-01 13:18:04 +0200

    creacion clase abstraca, modificacion AGENTS.md, reestructura de directorios

---
+++ src/piedra_papel_tijeras/models/jugada.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+
+
+class Jugada(ABC):
+    """clase abstracta de la que heredan Piedra, Papel y Tijeras
+    args:
+      name: str
+
+    abstractmethod:
+      __str__(self) -> str:"""
+
+    def __init__(self, name: str) -> None:
+        """define la instancia del objeto
+        args:
+          name: str
+        attributes:
+          self.name"""
+
+        self.name = name
+
+    @abstractmethod
+    def __str__(self) -> str:
+        """devuelve un f str con los datos name y type del objeto"""
+        return f"Name: {self.name}. Type: {type(self)}"
+
+    @abstractmethod
+    def vence_a(otra: Jugada) -> bool:
+        """Devuelve true si el objeto Jugada vence a otra
+        args:
+          otra: Jugada
+
+        return:
+          bool"""
+        pass
