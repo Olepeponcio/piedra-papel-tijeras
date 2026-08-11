9d8e59d9920da53436c04ade88caa914c63d4fef
Olepeponcio <olepepinsan@gmail.com>
2026-08-11 03:34:21 +0200

    interfaz de juego. implementación de clase ventanaDeJuego. Componentes y layouts

--- src/piedra_papel_tijeras/gui/ventana_de_juego.py
+++ src/piedra_papel_tijeras/gui/ventana_de_juego.py
@@ -6,6 +6,7 @@ from PySide6.QtWidgets import (
     QVBoxLayout,
     QWidget,
     QHBoxLayout,
+    QButtonGroup,
 )
 
 from piedra_papel_tijeras.models.tipo_jugada import TipoJugada
@@ -22,12 +23,12 @@ class VentanaDeJuego(QMainWindow):
         self.resize(600, 480)
 
         # jugada actual de la partida
-        self._jugada_actual: TipoJugada
+        self._jugada_actual: TipoJugada | None = None
 
         # imagenes permanecerán en el contenedor superpuestas y ocultas hasta evento
-        self.imagen_jugador_humano = ""
-        self.imagen_jugador_maquina = ""
-        self.imagen_resultado_jugada = ""
+        self.imagen_jugador_humano: QLabel | None = None
+        self.imagen_jugador_maquina: QLabel | None = None
+        self.imagen_resultado_jugada: QLabel | None = None
 
         # ejecutar la cascada de funciones
         self._crear_componentes()
@@ -38,13 +39,35 @@ class VentanaDeJuego(QMainWindow):
     def _crear_componentes(self) -> None:
         """creacion de los distintos componentes"""
 
-        self.piedra_btn = QPushButton(self._seleccion_jugada("Piedra"))
-        self.papel_btn = QPushButton(self._seleccion_jugada("Papel"))
-        self.tijeras_btn = QPushButton(self._seleccion_jugada("Tijeras"))
-        self.bo_btn = QPushButton("bo!")
+        # declarar los botones, activar setCheckable
+        self.piedra_btn = QPushButton("Piedra")
+        self.piedra_btn.setCheckable(True)
 
-    def _seleccion_jugada(self, jugada: str) -> None:
-        self._jugada_actual = TipoJugada(jugada)
+        self.papel_btn = QPushButton("Papel")
+        self.papel_btn.setCheckable(True)
+
+        self.tijeras_btn = QPushButton("Tijeras")
+        self.tijeras_btn.setCheckable(True)
+
+        # agrupar los botones
+        self.grupo_jugadas = QButtonGroup()
+        self.grupo_jugadas.addButton(self.piedra_btn)
+        self.grupo_jugadas.addButton(self.papel_btn)
+        self.grupo_jugadas.addButton(self.tijeras_btn)
+        # definir que solo un boton del grupo esté activado
+        self.grupo_jugadas.setExclusive(True)
+
+        self.bo_btn = QPushButton("bo!")
+        self.bo_btn.setCheckable(False)
+        self.bo_btn.setEnabled(False)
+
+        # inicializar las imagenes y añadir ruta
+        self.imagen_jugador_humano = QLabel()
+        self.imagen_jugador_humano.setPixmap(
+            "src\piedra_papel_tijeras\resources\btn_piedra.png"
+        )
+        self.imagen_jugador_maquina = QLabel()
+        self.imagen_resultado_jugada = QLabel()
 
     def _crear_layouts(self) -> None:
         """crea ventana principal y anida ventanas para albergar
@@ -57,11 +80,16 @@ class VentanaDeJuego(QMainWindow):
         self.layout_principal = QVBoxLayout()
 
         # 3. Layouts secundarios
-        self.layout_jugador_humano = QVBoxLayout()
-        self.layout_jugador_maquina = QVBoxLayout()
-        self.layout_resultado_jugada = QVBoxLayout()
+        self.layout_jugador_humano = QHBoxLayout()
+        self.layout_jugador_maquina = QHBoxLayout()
+        self.layout_resultado_jugada = QHBoxLayout()
+        
+        #un layout que anide jugador_humano, jugador_maquina y resultado_jugada
+        self.layout_jugadas = QHBoxLayout()
+        
+        
         self.layout_botones_jugadas = QHBoxLayout()
-        self.layout_boton_bo = QVBoxLayout()
+        self.layout_boton_bo = QHBoxLayout()
 
         # 4. Añadir componentes a layouts secundarios
         self.layout_botones_jugadas.addWidget(self.piedra_btn)
@@ -70,11 +98,16 @@ class VentanaDeJuego(QMainWindow):
         self.layout_boton_bo.addWidget(self.bo_btn)
 
         # 5. Anidar layouts
-        self.layout_principal.addChildLayout(self.layout_jugador_humano)
-        self.layout_principal.addChildLayout(self.layout_jugador_maquina)
-        self.layout_principal.addChildLayout(self.layout_resultado_jugada)
-        self.layout_principal.addChildLayout(self.layout_botones_jugadas)
-        self.layout_principal.addChildLayout(self.layout_boton_bo)
+        self.layout_jugadas.addLayout(self.layout_jugador_humano)
+        self.layout_jugadas.addLayout(self.layout_jugador_maquina)
+        self.layout_jugadas.addLayout(self.layout_resultado_jugada)
+        
+        # self.layout_principal.addLayout(self.layout_jugador_humano)
+        # self.layout_principal.addLayout(self.layout_jugador_maquina)
+        self.layout_principal.addLayout(self.layout_jugadas)
+        # self.layout_principal.addLayout(self.layout_resultado_jugada)
+        self.layout_principal.addLayout(self.layout_botones_jugadas)
+        self.layout_principal.addLayout(self.layout_boton_bo)
 
         # 6. Layout principal → QWidget
         self.widget_central.setLayout(self.layout_principal)
@@ -86,12 +119,40 @@ class VentanaDeJuego(QMainWindow):
         """genera todos los eventos disparados por los botones"""
         # seleccion de jugada mediante boton
         # si btn jugada está presionado, y se pulsa btn bo! descubrir imagen correspondiente a tipoJugada
-        self.bo_btn.clicked.connect(self._mostrar_imagen)
-        pass
+        self.piedra_btn.clicked.connect(
+            lambda: self._seleccion_jugada(TipoJugada.PIEDRA)
+        )
+        self.papel_btn.clicked.connect(lambda: self._seleccion_jugada(TipoJugada.PAPEL))
+
+        self.tijeras_btn.clicked.connect(
+            lambda: self._seleccion_jugada(TipoJugada.TIJERAS)
+        )
+
+        self.bo_btn.clicked.connect(lambda: self._ejectuar_ronda())
 
     def _configurar_estado_inicial(self) -> None:
         """prepara el entorno de juego"""
-        pass
+
+        # ocultar imagenes
+        # self.imagen_jugador_humano.hide()
+
+        # marcar botones como
 
     def _mostrar_imagen(self, jugada: str) -> None:
         pass
+
+    def _ejectuar_ronda(self) -> None:
+        #Validar que existe jugada humana
+        #desactivar botones
+        #solicitar ronda a Partida
+        #mostrar jugada humana
+        #mostrar jugada maquina
+        #Programar presentacion resultado
+        
+
+    def _seleccion_jugada(self, tipo_jugada: TipoJugada) -> None:
+        """Asigna valor al atributo y activa el botón bo!"""
+
+        self._jugada_actual = tipo_jugada
+        self.bo_btn.setEnabled(True)
+        self.bo_btn.setCheckable(True)
